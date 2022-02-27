53015024e7bf8de55d8cd03831cbc46f43c5cfd3
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-27 10:08:38 +0600

    setting all colro

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+const Colors = {
+  dark: {
+    backgroundColor: "#1E1E1E",
+    circle: "#252525",
+    text: "#F8F8F8",
+  },
+  light: {
+    backgroundColor: "#F8F8F8",
+    circle: "#FFF",
+    text: "#1E1E1E",
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: "rgba(256, 0, 256, 0.2)",
+  false: "rgba(0,0,0,0.1)",
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
